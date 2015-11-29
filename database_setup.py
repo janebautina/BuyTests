2575a0245cd288dd036b1e3f83fe9431e12f31bd
Evgeniya Bautina <janebarinova@gmail.com>
2015-11-29 01:08:27 -0800

    First commit. TODO: Add paypal

---
+++ database_setup.py
@@ -0,0 +1,19 @@
+import sys
+import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime, BLOB
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Orders(Base):
+  __tablename__ = 'orders'
+  id = Column(Integer, primary_key = True)
+  count = Column(Integer)
+  name = Column(String(80), nullable = False)
+  address = Column(String(80), nullable = False)
+  email = Column(String(80), nullable = False)
+
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.create_all(engine)
---
+++ orders.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify    
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Orders, Base
+
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+price = 89
+@app.route('/')
+def welcomePage():
+  return render_template('index.html')
+
+@app.route('/cart', methods=['GET', 'POST'])
+@app.route('/cart/', methods=['GET', 'POST']) 
+def newOrder():
+	if request.method == 'POST':
+	    newOrder = Orders(count =request.form['count'], name = request.form['name'], address = request.form['address'], email = request.form['email'])
+	    sum = price * newOrder.count
+	    session.add(newOrder)
+	    session.commit()
+	    return redirect(url_for('welcomePage'))
+	else:
+	    return render_template('cart.html', sum)
+
+if __name__ =='__main__':
+  app.secret_key = 'super_secret_key'
+  app.debug = True # reload if see any code changes
+  app.run(host = '0.0.0.0', port = 8080)
